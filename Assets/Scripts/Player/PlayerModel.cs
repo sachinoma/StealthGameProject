359e4467a3d8626d32189308ba28c9ec5d531e2e
Kao_TzuChiao <sachinoma0408@gmail.com>
2022-11-01 19:01:41 +0900

    PlayerInput.csからPlayerController.csに変更

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerModel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+    [SerializeField]
+    private Animator animator;
+    */
+
+    public void Movement()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Attack()
     {
-        
+
     }
 }
