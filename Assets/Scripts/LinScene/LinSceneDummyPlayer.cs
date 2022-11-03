194b00b3da8d87861f581dc9147cc6fc774f9bb0
hihilkh <hihilkh@gmail.com>
2022-11-03 19:21:04 +0900

    仮のプレイヤーコントローラーを追加する

--- Assets/Scripts/LinScene/LinSceneDummyPlayer.cs
+++ Assets/Scripts/LinScene/LinSceneDummyPlayer.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class LinSceneDummyPlayer : MonoBehaviour
+public class LinSceneDummyPlayer : MonoBehaviour, PlayerInputAction.IPlayerActions
 {
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotateSpeed;
+
+    private Vector2 moveInput = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
+        PlayerInputAction userInput = new PlayerInputAction();
+        userInput.Player.SetCallbacks(this);
+        userInput.Player.Enable();
+
         StartCoroutine(Step());
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
     }
 
     private IEnumerator Step()
@@ -23,4 +34,39 @@ public class LinSceneDummyPlayer : MonoBehaviour
             VisualRangeGenerator.Instance?.Generate(transform.position, 1);
         }
     }
+
+    private void Move()
+    {
+        Vector3 moveVector = Vector3.zero;
+        moveVector += transform.forward * moveInput.y * _moveSpeed * Time.deltaTime;
+        moveVector += transform.right * moveInput.x * _moveSpeed * Time.deltaTime;
+
+        transform.position += moveVector;
+    }
+
+    #region PlayerInputAction.IPlayerActions
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 value = context.ReadValue<Vector2>();
+        Vector3 rotateVector = Vector3.zero;
+        rotateVector.y += value.x * _rotateSpeed * Time.deltaTime;
+
+        transform.Rotate(rotateVector);
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+    }
+
+    #endregion
 }
