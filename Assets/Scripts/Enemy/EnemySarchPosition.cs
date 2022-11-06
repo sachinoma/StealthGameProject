5807fcaf304875b9be3763a61a7a391abe70ea17
BoxS0225 <ryota0225s@gmail.com>
2022-11-06 12:24:27 +0900

    EnemyModel.csの変更

--- Assets/Scripts/Enemy/EnemySarchPosition.cs
+++ Assets/Scripts/Enemy/EnemySarchPosition.cs
@@ -9,7 +9,7 @@ public class EnemySarchPosition : MonoBehaviour
     private void OnDrawGizmos()
     {
 
-        Handles.color = new Color(1f, 1f, 1f, 0.3f);
+        Handles.color = new Color(1f, 1f, 1f, 0.5f);
         Handles.DrawSolidArc(transform.position, Vector3.up,transform.forward, 360f, 1f);
     }
 }
