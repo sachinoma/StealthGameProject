dca16551fcc78c9b4b64a9ce98502eec6032c9da
Kao_TzuChiao <sachinoma0408@gmail.com>
2022-11-27 16:48:01 +0900

    黒いFogの機能を調整しました。（距離を動的に調整可能） トリガーも追加しました。

--- Assets/Scripts/KouScene/MicRange.cs
+++ Assets/Scripts/KouScene/MicRange.cs
@@ -7,19 +7,35 @@ using UnityEngine.UI;
 
 public class MicRange : MonoBehaviour
 {
-    public Slider volumeSlider;
-    [SerializeField, Range(0f, 20f)] float m_gain = 1f; // 音量に掛ける倍率
-    float m_volumeRate; // 音量(0-1)
+    [Header("音量に掛ける倍率")]
+    [SerializeField, Range(0f, 20f)] private float m_gain = 1f; // 音量に掛ける倍率
+    private float m_volumeRate; // 音量(0-1)
+
+    [Header("音量チェックできる最小値")]
+    [SerializeField, Range(0f, 1.0f)] private float _minRate = 0.2f;
+    [Header("黒いFogが戻る速さ")]
+    [SerializeField, Range(0f, 2.0f)] private float _rollBackSpeed = 0.5f;
+
+    [Header("Fogの最大範囲(0.5~2.0)")]
+    [SerializeField, Range(0.5f, 2.0f)] private float _maxRange = 1.0f;
+    [Header("Fogの最小範囲(0.5~2.0)")]
+    [SerializeField, Range(0.5f, 2.0f)] private float _minRange = 1.0f;
+
+
+    //黒いFog
+    private float _fogMax = 0.18f;
+    private float _fogMin = 0.03f;
+
+    //黒い丸コライダーとメッシュ
+    private float _circleBlackMax = 4.5f;
+    private float _circleBlackMin = 1.0f;
 
     [SerializeField] private GameObject _circleBlack;
     private Vector3 _circleBlackBasic = new Vector3(1, 1, 1);
     private float _circleBlackValue = 1.0f;
 
-    // Use this for initialization
     void Start()
     {
-
-
         AudioSource aud = GetComponent<AudioSource>();
         if((aud != null) && (Microphone.devices.Length > 0)) // オーディオソースとマイクがある
         {
@@ -29,9 +45,9 @@ public class MicRange : MonoBehaviour
             aud.clip = Microphone.Start(devName, true, 2, minFreq); // 音の大きさを取るだけなので最小サンプリングで十分
             aud.Play(); //マイクをオーディオソースとして実行(Play)開始
         }
+        RenderSettings.fogDensity = _fogMax / _minRange;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -39,32 +55,31 @@ public class MicRange : MonoBehaviour
 
     void FixedUpdate()
     {
-        //Debug.Log(m_volumeRate);
-        volumeSlider.value = m_volumeRate;
-        //0 0.1   1 0.03
-        //RenderSettings.fogDensity -= 0.0005f;
-        if(m_volumeRate > 0.2f)
+        if(VolumeCheck())
         {
-            RenderSettings.fogDensity = FogValue(m_volumeRate, 0.18f, 0.03f);
-            _circleBlackValue = CircleBlackValue(m_volumeRate, 4.5f, 1.0f);
+            FogSetting(_fogMax/ _minRange, _fogMin/ _maxRange);
+            CircleBlackSetting(_circleBlackMax* _maxRange, _circleBlackMin* _minRange);
         }
-        
-        if(RenderSettings.fogDensity < 0.18f)
-        {
-            RenderSettings.fogDensity += 0.001f;
-        }
-        if(_circleBlackValue >1.0f)
-        {
-            _circleBlackValue -= 0.019f;
-        }
-        _circleBlack.transform.localScale = _circleBlackValue * _circleBlackBasic;
 
-        Debug.Log(RenderSettings.fogDensity);
+        FogRollBack(_fogMax / _minRange, _fogMin / _maxRange);
+        CircleBlackRollBack(_circleBlackMax * _maxRange, _circleBlackMin * _minRange);
     }
 
+    //音量チェック
+    private bool VolumeCheck()
+    {
+        return m_volumeRate > _minRate;
+    }
+
+    #region 黒いFog  
+    public void FogSetting(float max, float min)
+    {
+        RenderSettings.fogDensity = FogValue(m_volumeRate, max, min);
+    }
+
+    // 黒いFogの増減、maxは一番黒い,minは一番遠い
     float FogValue(float num , float max , float min)
     {
-        //max + (num * -0.07f)
         float _tmpValue = max + (num * -(max - min));
         if(RenderSettings.fogDensity > _tmpValue)
         {
@@ -73,6 +88,25 @@ public class MicRange : MonoBehaviour
         return RenderSettings.fogDensity;
     }
 
+    public void FogRollBack(float max, float min)
+    {
+        const float _fogSpeedSet = 150.0f;
+
+        if(RenderSettings.fogDensity < max)
+        {
+            RenderSettings.fogDensity += ((max - min)/ _fogSpeedSet) * _rollBackSpeed;
+            //RenderSettings.fogDensity += 0.001f * _rollBackSpeed;
+        }
+    }
+    #endregion
+
+    #region 黒い丸コライダーとメッシュ
+    public void CircleBlackSetting(float max, float min)
+    {
+        _circleBlackValue = CircleBlackValue(m_volumeRate, max, min);
+    }
+    // 黒い丸コライダーとメッシュの増減、maxは一番遠い,minは一番近い
+    // FogValueと真逆
     float CircleBlackValue(float num, float max, float min)
     {
         float _tmpValue = min + (num * (max - min));
@@ -83,6 +117,18 @@ public class MicRange : MonoBehaviour
         return _circleBlackValue;
     }
 
+    public void CircleBlackRollBack(float max, float min)
+    {
+        const float _circleBlackSpeedSet = 140.0f;
+
+        if(_circleBlackValue > min)
+        {
+            _circleBlackValue -= ((max - min) / _circleBlackSpeedSet) * _rollBackSpeed;
+            //_circleBlackValue -= 0.025f * _rollBackSpeed;
+        }
+        _circleBlack.transform.localScale = _circleBlackValue * _circleBlackBasic;
+    }
+    #endregion
 
     // オーディオが読まれるたびに実行される
     private void OnAudioFilterRead(float[] data, int channels)
