2e0de0ddbf1c50cf13f4cb73d7d6a3e6dfee4c47
Kao_TzuChiao <sachinoma0408@gmail.com>
2022-12-05 17:13:35 +0900

    マイク機能がリスタートする時上手く作動できないバグを治りました。 音の範囲が広がることによって敵もプレイヤーを検知できる。

--- Assets/Scripts/KouScene/MicRange.cs
+++ Assets/Scripts/KouScene/MicRange.cs
@@ -7,9 +7,13 @@ using UnityEngine.UI;
 
 public class MicRange : MonoBehaviour
 {
+    //マイク機能
+    private readonly int _sampleNum = (2 << 9); // サンプリング数は2のN乗(N=5-12)
     [Header("音量に掛ける倍率")]
-    [SerializeField, Range(0f, 20f)] private float m_gain = 1f; // 音量に掛ける倍率
-    private float m_volumeRate; // 音量(0-1)
+    [SerializeField, Range(0f, 3000f)] float m_gain = 1500f; // 倍率
+    AudioSource m_source;
+    float[] _currentValues;
+    float _volumeRate;
 
     [Header("音量チェックできる最小値")]
     [SerializeField, Range(0f, 1.0f)] private float _minRate = 0.2f;
@@ -27,8 +31,16 @@ public class MicRange : MonoBehaviour
     private float _fogMin = 0.03f;
 
     //黒い丸コライダーとメッシュ
-    private float _circleBlackMax = 4.5f;
+    [Header("SoundFogRangeの拡大倍率(1.5~3.0)")]
+    [SerializeField, Range(1.5f, 3.0f)] private float _circleBlackMax = 2.0f;
     private float _circleBlackMin = 1.0f;
+
+    //丸コライダー（Enemyから検出用）
+    [SerializeField] private GameObject _circleForEnemy;
+    [Header("敵から検知できるコライダーの大きさ(3.0~8.0)")]
+    [SerializeField, Range(3.0f, 8.0f)] private float _circleForEnemyScaleValue = 5.0f;
+    private Vector3 _circleForEnemyScale;
+
 
     [SerializeField] private GameObject _circleBlack;
     private Vector3 _circleBlackBasic = new Vector3(1, 1, 1);
@@ -36,45 +48,71 @@ public class MicRange : MonoBehaviour
 
     void Start()
     {
-        AudioSource aud = GetComponent<AudioSource>();
-        if((aud != null) && (Microphone.devices.Length > 0)) // オーディオソースとマイクがある
-        {
-            string devName = Microphone.devices[0]; // 複数見つかってもとりあえず0番目のマイクを使用
-            int minFreq, maxFreq;
-            Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // 最大最小サンプリング数を得る
-            aud.clip = Microphone.Start(devName, true, 2, minFreq); // 音の大きさを取るだけなので最小サンプリングで十分
-            aud.Play(); //マイクをオーディオソースとして実行(Play)開始
-        }
+        MicStart();
         RenderSettings.fogDensity = _fogMax / _minRange;
+        _circleForEnemyScale = new Vector3(_circleForEnemyScaleValue, _circleForEnemyScaleValue, _circleForEnemyScaleValue);
     }
 
     void Update()
     {
-        
+        MicUpdate();
+
+        if(VolumeCheck())
+        {
+            FogSetting(_fogMax / _minRange, _fogMin / _maxRange);
+            CircleBlackSetting(_circleBlackMax * _maxRange, _circleBlackMin * _minRange);
+        }
+        FogRollBack(_fogMax / _minRange, _fogMin / _maxRange);
+        CircleBlackRollBack(_circleBlackMax * _maxRange, _circleBlackMin * _minRange);
     }
 
     void FixedUpdate()
     {
-        if(VolumeCheck())
-        {
-            FogSetting(_fogMax/ _minRange, _fogMin/ _maxRange);
-            CircleBlackSetting(_circleBlackMax* _maxRange, _circleBlackMin* _minRange);
-        }
+        
+    }
 
-        FogRollBack(_fogMax / _minRange, _fogMin / _maxRange);
-        CircleBlackRollBack(_circleBlackMax * _maxRange, _circleBlackMin * _minRange);
+    //マイク設定
+    public void MicStart()
+    {
+        m_source = GetComponent<AudioSource>();
+        _currentValues = new float[_sampleNum];
+        if((m_source != null) && (Microphone.devices.Length > 0)) // オーディオソースとマイクがある
+        {
+            string devName = Microphone.devices[0]; // 複数見つかってもとりあえず0番目のマイクを使用
+            int minFreq, maxFreq;
+            Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // 最大最小サンプリング数を得る
+            int ms = minFreq / _sampleNum; // サンプリング時間を適切に取る
+            m_source.loop = true; // ループにする
+            m_source.clip = Microphone.Start(devName, true, ms, minFreq); // clipをマイクに設定
+            while(!(Microphone.GetPosition(devName) > 0)) { } // きちんと値をとるために待つ
+            Microphone.GetPosition(null);
+            m_source.Play();
+        }
+    }
+
+    public void MicUpdate()
+    {
+        m_source.GetSpectrumData(_currentValues, 0, FFTWindow.Hamming);
+        float sum = 0f;
+        for(int i = 0; i < _currentValues.Length; ++i)
+        {
+            sum += _currentValues[i]; // データ（周波数帯ごとのパワー）を足す
+        }
+        // データ数で割ったものに倍率をかけて音量とする
+        _volumeRate = Mathf.Clamp01(sum * m_gain / (float)_currentValues.Length);
+        Debug.Log(_volumeRate);
     }
 
     //音量チェック
     private bool VolumeCheck()
     {
-        return m_volumeRate > _minRate;
+        return _volumeRate > _minRate;
     }
 
     #region 黒いFog  
     public void FogSetting(float max, float min)
     {
-        RenderSettings.fogDensity = FogValue(m_volumeRate, max, min);
+        RenderSettings.fogDensity = FogValue(_volumeRate, max, min);
     }
 
     // 黒いFogの増減、maxは一番黒い,minは一番遠い
@@ -90,11 +128,15 @@ public class MicRange : MonoBehaviour
 
     public void FogRollBack(float max, float min)
     {
-        const float _fogSpeedSet = 150.0f;
+        const float _fogSpeedSet = 1.5f;
+
+        _circleForEnemy.transform.localScale = new Vector3(0, 0, 0);
 
         if(RenderSettings.fogDensity < max)
         {
-            RenderSettings.fogDensity += ((max - min)/ _fogSpeedSet) * _rollBackSpeed;
+            RenderSettings.fogDensity += ((max - min)/ _fogSpeedSet) * _rollBackSpeed * Time.deltaTime;
+            _circleForEnemy.transform.localScale = _circleForEnemyScale;
+
             //RenderSettings.fogDensity += 0.001f * _rollBackSpeed;
         }
     }
@@ -103,7 +145,7 @@ public class MicRange : MonoBehaviour
     #region 黒い丸コライダーとメッシュ
     public void CircleBlackSetting(float max, float min)
     {
-        _circleBlackValue = CircleBlackValue(m_volumeRate, max, min);
+        _circleBlackValue = CircleBlackValue(_volumeRate, max, min);
     }
     // 黒い丸コライダーとメッシュの増減、maxは一番遠い,minは一番近い
     // FogValueと真逆
@@ -119,27 +161,14 @@ public class MicRange : MonoBehaviour
 
     public void CircleBlackRollBack(float max, float min)
     {
-        const float _circleBlackSpeedSet = 140.0f;
+        const float _circleBlackSpeedSet = 1.4f;
 
         if(_circleBlackValue > min)
         {
-            _circleBlackValue -= ((max - min) / _circleBlackSpeedSet) * _rollBackSpeed;
+            _circleBlackValue -= ((max - min) / _circleBlackSpeedSet) * _rollBackSpeed * Time.deltaTime;
             //_circleBlackValue -= 0.025f * _rollBackSpeed;
         }
         _circleBlack.transform.localScale = _circleBlackValue * _circleBlackBasic;
     }
     #endregion
-
-    // オーディオが読まれるたびに実行される
-    private void OnAudioFilterRead(float[] data, int channels)
-    {
-        float sum = 0f;
-        for(int i = 0; i < data.Length; ++i)
-        {
-            sum += Mathf.Abs(data[i]); // データ（波形）の絶対値を足す
-        }
-        // データ数で割ったものに倍率をかけて音量とする
-        m_volumeRate = Mathf.Clamp01(sum * m_gain / (float)data.Length);
-        if(m_volumeRate < 0.001f) { m_volumeRate = 0; }
-    }
 }
