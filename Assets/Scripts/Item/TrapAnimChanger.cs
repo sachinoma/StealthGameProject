cc122832decb741d21e81dcaf04e6d4b069ec256
DESKTOP-R0JRRCE\Leo <hypixel.block@gmail.com>
2022-12-13 11:07:42 +0900

    トラップの　・テストモデル　・テストアニメ　を追加。 ItemScriptを使わなさそうなのでReNameしてトラップアニメのScriptに変更。

---
+++ Assets/Scripts/Item/TrapAnimChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapAnimChanger : MonoBehaviour
+{
+    Animator _trapAnim;
+
+    void Awake()
+    {
+        _trapAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _trapAnim.SetBool("IsOn", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _trapAnim.SetBool("IsOn", false);
+    }
+}
