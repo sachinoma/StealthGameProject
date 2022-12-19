4c89bf585a62124b5dd8eebf3c511252e084baed
hihilkh <hihilkh@gmail.com>
2022-12-19 10:24:21 +0900

    PlayerMovementスクリプトを改良

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,12 +10,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _maxSpeed = 3.0f;
     [SerializeField] private float _acceleration = 10.0f;
     [SerializeField] private float _rotateSpeed = 15.0f;
-    [SerializeField] private float _minMovementThreshold = 0.1f;
+
+    [SerializeField, Range(0.0f, 1.0f), Tooltip("小さすぎる移動を防ぐ")]
+    private float _minMovementThreshold = 0.1f;
+    private float _minMovementThresholdSquare;  // _minMovementThresholdの平方
 
     private Vector2 _targetMovement;        // 希望な移動。カメラ空間座標(2Dのみ)。magnitudeの範囲 : [0, 1]
-    private Vector3 _lastInputVelocity;     // _targetMovementによって計算後の入力速度。実際の速度ではない(壁をぶつけるとか、実際の速度が変わる場合もある)
+    private Vector3 _inputVelocity;         // _targetMovementによって計算後の入力速度。実際の速度ではない(壁をぶつけるとか、実際の速度が変わる場合もある)
 
     private bool _isMovementActive = true;
+
+    private void Awake()
+    {
+        UpdateMinMovementThresholdSquare();
+    }
+
+    private void OnValidate()
+    {
+        UpdateMinMovementThresholdSquare();
+    }
 
     private void Update()
     {
@@ -26,13 +39,22 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 targetMovement_WorldSpace = ConvertToWorldSpaceTargetMovement(_targetMovement);
         Move(targetMovement_WorldSpace);
-        Rotate(_lastInputVelocity);
+        Rotate(_inputVelocity);
+    }
+
+    private void UpdateMinMovementThresholdSquare()
+    {
+        _minMovementThresholdSquare = _minMovementThreshold * _minMovementThreshold;
     }
 
     private Vector3 ConvertToWorldSpaceTargetMovement(Vector2 targetMovement_2DCameraSpace)
     {
-        if(Mathf.Abs(targetMovement_2DCameraSpace.x) < _minMovementThreshold &&
-           Mathf.Abs(targetMovement_2DCameraSpace.y) < _minMovementThreshold)
+        if(targetMovement_2DCameraSpace == Vector2.zero)
+        {
+            return Vector3.zero;
+        }
+
+        if(targetMovement_2DCameraSpace.sqrMagnitude < _minMovementThresholdSquare)
         {
             return Vector3.zero;
         }
@@ -43,22 +65,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move(Vector3 targetMovement_WorldSpace)
     {
-        if(targetMovement_WorldSpace == Vector3.zero && _lastInputVelocity == Vector3.zero)
+        if(targetMovement_WorldSpace == Vector3.zero && _inputVelocity == Vector3.zero)
         {
             return;
         }
 
         Vector3 targetVelocity = targetMovement_WorldSpace * _maxSpeed;
-        if (targetVelocity == Vector3.zero)
-        {
-            _lastInputVelocity = Vector3.MoveTowards(_lastInputVelocity, targetVelocity, _acceleration * Time.deltaTime);
-        }
-        else
-        {
-            _lastInputVelocity = Vector3.RotateTowards(_lastInputVelocity, targetVelocity, _rotateSpeed * Time.deltaTime, _acceleration * Time.deltaTime);
-        }
-
-        _characterController.Move(_lastInputVelocity * Time.deltaTime);
+        _inputVelocity = Vector3.RotateTowards(_inputVelocity, targetVelocity, _rotateSpeed * Time.deltaTime, _acceleration * Time.deltaTime);
+        _characterController.Move(_inputVelocity * Time.deltaTime);
     }
 
     private void Rotate(Vector3 forward)
@@ -79,7 +93,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if(_isMovementActive)
         {
-            return _lastInputVelocity.magnitude / _maxSpeed;
+            return _inputVelocity.magnitude / _maxSpeed;
         }
         else
         {
@@ -92,7 +106,8 @@ public class PlayerMovement : MonoBehaviour
         _isMovementActive = isActive;
         if(!isActive)
         {
-            _lastInputVelocity = Vector3.zero;
+            _targetMovement = Vector2.zero;
+            _inputVelocity = Vector3.zero;
         }
     }
 }
