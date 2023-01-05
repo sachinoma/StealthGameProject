58df14c4878f80b4cb0da2da5f6c016216bc9ce0
Kao_TzuChiao <sachinoma0408@gmail.com>
2023-01-05 21:58:24 +0900

    自動ドアの導入。

---
+++ Assets/Scripts/MapObjects/AutoDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    Animator _animator;
+   
+    void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(Tag.Player))
+        {
+            _animator.SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag(Tag.Player))
+        {
+            _animator.SetBool("isOpen", false);
+        }
+    }
+}
